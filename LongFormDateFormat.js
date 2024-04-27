288059efc533fd3ec743fc9a0a7da1d8a37e51a6
KYOCUTI <160293377+KYOCUTI@users.noreply.github.com>
2024-04-27 01:25:34 -0600

    LongFormDateFormat.js

--- LongFormDateFormat.js
+++ LongFormDateFormat.js
@@ -1,14 +1,18 @@
 /*
- * This JS script formats the date to be displayed in the
- * format: "Saturday, January 01, 2024" / {Day of the Week}, {Month} {Day}, {Year}
- * This format is human-readable and commonly used in written English.
- */
+This JS script formats the date to be displayed in the format: "{Day of the Week}, {Month} {Day}, {Year}"
+Example: Saturday, January 01, 2024
+This format is human-readable and commonly used in written English.
+*/
 
+// Get current date
 var currentDate = new Date();
 
+// Define days and months
 var days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
 var months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
 
+// Format date
 var formattedDate = days[currentDate.getDay()] + ', ' + months[currentDate.getMonth()] + ' ' + currentDate.getDate() + ', ' + currentDate.getFullYear();
 
+// Return formatted date
 formattedDate;
